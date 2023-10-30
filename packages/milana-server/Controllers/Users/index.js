29e1d0e0730240e27987c8ac6c74b05ef045662c
3bu1 <thrihbuvan.d@mannkey.com>
2023-10-30 16:33:24 +0530

    feat(BE): created User entity

--- packages/milana-server/Controllers/Users/index.js
+++ packages/milana-server/Controllers/Users/index.js
@@ -50,4 +50,52 @@ const saveUser = async (req, res, status) => {
 		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ReasonPhrases.INTERNAL_SERVER_ERROR);
 	}
 };
-module.exports = { getUser, saveUser };
+/**
+ * @description Deactivate the user account
+ * @param {express.Request} req - The express request object
+ * @param {express.Response} res - The express response object
+ * @returns {void} - Returns response back to the restfull client
+ */
+const deactivate = async (req, res) => {
+	const { error, value } = userSchema.validate(req.body);
+	try {
+		if (error) {
+			throw new Error(err);
+		}
+		const user = new User(value);
+		const deactivateUser = await user.toggleAccountStatus(true);
+		if (deactivateUser) {
+			return res.status(StatusCodes.NO_CONTENT).send(ReasonPhrases.NO_CONTENT);
+		} else {
+			throw new Error('Invalid Operation: Unable to deactivate the user');
+		}
+	} catch (err) {
+		logger.error(err);
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+	}
+};
+/**
+ * @description Reactivate the user account
+ * @param {express.Request} req - The express request object
+ * @param {express.Response} res - The express response object
+ * @returns {void} - Returns response back to the restfull client
+ */
+const activate = async (req, res) => {
+	const { error, value } = userSchema.validate(req.body);
+	try {
+		if (error) {
+			throw new Error(err);
+		}
+		const user = new User(value);
+		const deactivateUser = await user.toggleAccountStatus(false);
+		if (deactivateUser) {
+			return res.status(StatusCodes.OK).send(ReasonPhrases.OK);
+		} else {
+			throw new Error('Invalid Operation: Unable to activate the user');
+		}
+	} catch (err) {
+		logger.error(err);
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+	}
+};
+module.exports = { getUser, saveUser, deactivate, activate };
